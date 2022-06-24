e9ca3363c6e7c2d8cf9a3d253bfe3f5b69af9b5b
AlbertKloster <albert.kloster@gmail.com>
2022-06-24 15:00:22 +0200

    The program should create a 4-digit secret code, and the player should try to guess it on the first try. The program should give a grade to evaluate how accurate the player was.

--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -1,20 +1,14 @@
 package bullscows;
 
-import java.util.Random;
+
+import static bullscows.Utils.*;
 
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("The secret code is prepared: ****.");
-        Random random = new Random();
-        int secretCode = 1000;
-        for (int i = 0; i < 2; i++) {
-            secretCode = random.nextInt(10000 - 1000) + 1000;
-            System.out.printf("\nTurn %d. Answer:\n", i + 1);
-            System.out.println(secretCode);
-            System.out.printf("Grade: %d bulls.\n", 4 * i);
-        }
-        System.out.printf("Congrats! The secret code is %d.\n", secretCode);
-
+        String secretCode = getSecretCode(4);
+        String number = getInt(4);
+        int bulls = getBulls(secretCode, number);
+        int cows = getCows(secretCode, number);
+        printResult(secretCode, bulls, cows);
     }
 }
--- Bulls and Cows/task/src/bullscows/Utils.java
+++ Bulls and Cows/task/src/bullscows/Utils.java
@@ -1,15 +1,63 @@
 package bullscows;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Utils {
     private static Scanner SC = new Scanner(System.in);
 
-    public static int getInt() {
+    public static String getInt(int digits) {
         String input = SC.nextLine();
-        if (input.matches("\\d{4}"))
-            return Integer.parseInt(input);
+        String regex = String.format("\\d{%d}", digits);
+        if (input.matches(regex))
+            return input;
         else
-            return -1;
+            return null;
     }
+
+    public static String getSecretCode(int digits) {
+        Random random = new Random();
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < digits; i++) {
+            builder.append(random.nextInt(10));
+        }
+        return builder.toString();
+    }
+
+    public static int getBulls(String secretCode, String number) {
+        int bulls = 0;
+        for (int i = 0; i < secretCode.length(); i++)
+            if (secretCode.charAt(i) == number.charAt(i))
+                bulls++;
+        return bulls;
+    }
+
+    public static int getCows(String secretCode, String number) {
+        int cows = 0;
+        String secretCodeXorString = getXorString(secretCode, number);
+        String numberXorString = getXorString(number, secretCode);
+        for (char ch : numberXorString.toCharArray())
+            if (secretCodeXorString.contains(String.valueOf(ch)))
+                cows++;
+        return cows;
+    }
+
+    private static String getXorString (String string, String refString) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < string.length(); i++)
+            if (string.charAt(i) != refString.charAt(i))
+                builder.append(string.charAt(i));
+        return builder.toString();
+    }
+
+    public static void printResult(String secretCode, int bulls, int cows) {
+        if (bulls == 0 && cows == 0)
+            System.out.printf("Grade: None. The secret code is %s.\n", secretCode);
+        else if (bulls == 0)
+            System.out.printf("Grade: %d cow(s). The secret code is %s.\n", cows, secretCode);
+        else if (cows == 0)
+            System.out.printf("Grade: %d bull(s). The secret code is %s.\n", bulls, secretCode);
+        else
+            System.out.printf("Grade: %d bull(s) and %d cow(s). The secret code is %s.", bulls, cows, secretCode);
+    }
+
 }
